7d687f1f8725f8412cd60f64ef904f727e7589ec
adam270 <musedadam95@gmail.com>
2023-10-01 15:49:20 -0600

    updated api types for pokemon endpoint

--- server/api/pokemon/index.post.ts
+++ server/api/pokemon/index.post.ts
@@ -1,15 +1,12 @@
 import { defineEventHandler } from "h3";
-import type {
-  NamedAPIResourceList,
-  NamedAPIResource,
-} from "~/types/pokemon/poke-api";
+import type { NamedAPIResourceList } from "~/types/pokemon/poke-api";
 import type { Pokemon } from "~/types/pokemon/pokemon";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { url } = body;
 
-  const getPokemon: NamedAPIResourceList | NamedAPIResource = await $fetch(url);
+  const getPokemon: NamedAPIResourceList | Pokemon = await $fetch(url);
   const getDetails = getPokemon.results ?? false;
   const pokemon: Array<Pokemon> = [];
   if (getDetails) {
